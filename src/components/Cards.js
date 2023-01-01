e953517efeaeec931c268da9f878d53b0d875778
obaidurRehman97 <obaid.rehman@shopdev.co>
2023-01-01 19:20:53 +0500

    feat: fetching products from api

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,30 +1,53 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css';
-import Carddata from './Carddata';
+import React, { useEffect, useState } from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
+import axios from "axios";
 function Cards() {
-    return (
-        <div className='cards'>
-            <h1>Check out these Destination</h1>
-            <div className='cards__container'>
-                <div className='cards__wrapper'>
-                    <ul className='cards__items'>
-                    {
-           Carddata.map ((val )=> {
-              return (
-                <CardItem
-                imgsrc={val.imgsrc}
-                text = {val.text}
-                label = {val.label}
-                path= {val.path}/ >
-              );
-            })
-           }
-                  </ul>
-                </div>
-            </div>
+  const [cardData, setCardData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const fetchCardData = () => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => {
+        console.log(res);
+        setCardData(res?.data?.products);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(true);
+      });
+  };
+  useEffect(() => {
+    fetchCardData();
+  }, []);
+  return (
+    <div className="cards">
+      <h1>Check out these Products</h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            {cardData &&
+              cardData.length > 0 &&
+              cardData?.map((val) => {
+                console.log(val.price);
+                return (
+                  <CardItem
+                    imgsrc={val.images[0]}
+                    name={val.title}
+                    brand={val.brand}
+                    price={val.pice || "$$$"}
+                    description={val.description}
+                    path={`/product/${val.id}`}
+                  />
+                );
+              })}
+          </ul>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
